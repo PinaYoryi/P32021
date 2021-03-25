02e8b6bcb57836b540e2d50adb8a3c7f64486ae4
Daniel Cortijo <dcortijo@ucm.es>
2021-03-25 17:58:12 +0100

    Hecho dependencias con libs, dlls e includes
    Se debe hacer con un .bat
    Debemos quitar x86 y cambiar el destino de Ogre.log

--- src/PiñaMotor/Source.cpp
+++ src/PiñaMotor/Source.cpp
@@ -1,3 +1,14 @@
+#include <OgreRoot.h>
+
+#if (defined _DEBUG) || !(defined _WIN32) //<-- Ya no lo tenemos en teoría
 int main() {
-	return 0;
+#else
+#include <windows.h>
+int WINAPI
+WinMain(HINSTANCE zhInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow) {
+#endif
+    Ogre::Root* root;
+    root = new Ogre::Root();
+
+    return 0;
 }
