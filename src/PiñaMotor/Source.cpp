4fe7d8df6d4fb47cf0db7656f46539127f660678
Daniel González Cerdeiras <daniel20@ucm.es>
2021-03-27 14:02:28 +0100

    Añadida detección de leaks de memoria
    Por si acaso.

--- src/PiñaMotor/Source.cpp
+++ src/PiñaMotor/Source.cpp
@@ -1,6 +1,8 @@
 #include "MotorLoop.h"
 
 int main() {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // Check Memory Leaks
+
 	MotorLoop* loop = MotorLoop::getInstance();
 	loop = MotorLoop::getInstance();
 	loop->startLoop();
