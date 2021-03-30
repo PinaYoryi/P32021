41859382da64927c666c9c7dfa43e34e4cdc1b1d
Xexu98 <jesusf05@ucm.es>
2021-03-30 12:14:33 +0200

    Añadido Fmod a release

--- src/PiñaMotor/Source.cpp
+++ src/PiñaMotor/Source.cpp
@@ -1,11 +1,5 @@
 #include <OgreRoot.h>
 
-#include <fmod.hpp>
-#include <fmod_errors.h>
-#include <iostream>
-#include <thread>
-#include <cstdio>
-#include <conio.h>
 
 #if (defined _DEBUG) || !(defined _WIN32) //<-- Ya no lo tenemos en teoría
 int main() {
