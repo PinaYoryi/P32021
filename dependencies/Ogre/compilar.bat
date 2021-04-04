7f67bd1c24082489c44c547921354b05763180ae
Daniel Cortijo <dcortijo@ucm.es>
2021-04-05 00:21:27 +0200

    .bat actualizado con errores
    La primera compilación da errores, en la segunda va perfectamente

--- dependencies/Ogre/compilar.bat
+++ dependencies/Ogre/compilar.bat
@@ -1,11 +1,24 @@
-rmdir /S /Q Build
+REM Si no hay dlls compilados en Build, compilar
+IF not exist Build/bin/release/OgreMain.dll (
+IF not exist Build/bin/debug/OgreMain_d.dll (
+rmdir /S /Q Build 
 mkdir Build
 cd Build
 cmake ../Src
-cmake --build . --config release
+cmake --build . --config release 
 cmake --build . --config debug
-cd bin/debug
+cd ..
+)
+)
+REM Hacer bin si no existe ya
+cd ../..
+IF not exist bin (
+mkdir bin 
+)
+REM Copiar los dlls pertinentes
+cd dependencies/Ogre/Build/bin/debug
 copy /Y OgreMain_d.dll "../../../../../bin/OgreMain_d.dll"
 cd ../release
 copy /Y OgreMain.dll "../../../../../bin/OgreMain.dll"
 copy /Y zlib.dll "../../../../../bin/zlib.dll"
+echo DONE
