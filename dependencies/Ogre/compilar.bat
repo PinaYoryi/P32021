557b40230071380de894bd0b6995229d76acc0c9
Daniel Cortijo <dcortijo@ucm.es>
2021-04-05 18:35:24 +0200

    Mejoradas condiciones del .bat

--- dependencies/Ogre/compilar.bat
+++ dependencies/Ogre/compilar.bat
@@ -1,7 +1,9 @@
 REM Si no hay dlls compilados en Build, compilar
-IF not exist ../../bin/OgreMain.dll (
-IF not exist ../../bin/OgreMain_d.dll (
-IF not exist ../../bin/zlib.dll (
+if not exist ../../bin/OgreMain.dll goto foo
+if not exist ../../bin/OgreMain_d.dll goto foo
+if not exist ../../bin/zlib.dll goto foo
+goto afterfoo
+:foo
 rmdir /S /Q Build 
 mkdir Build
 cd Build
@@ -10,15 +12,13 @@ cmake --build . --config release
 cmake --build . --config debug
 cd ..
 echo CMADE
-)
-)
-)
+:afterfoo
 REM Hacer bin si no existe ya
 cd ../..
 IF not exist bin (
 mkdir bin 
-)
 echo BINDONE
+)
 REM Copiar los dlls pertinentes
 cd dependencies/Ogre/Build/bin/debug
 copy /Y OgreMain_d.dll "../../../../../bin/OgreMain_d.dll"
