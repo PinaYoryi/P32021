745aef09c441f5dab37730e71c126783fa3823f3
Daniel Cortijo <dcortijo@ucm.es>
2021-04-05 18:02:34 +0200

    Actualizado .bat con distintas condiciones
    Preguntaremos al profe si los errores son normales de ogre (pues son de cosas de ogre, pero no sabemos si deberían salir)

--- dependencies/Ogre/compilar.bat
+++ dependencies/Ogre/compilar.bat
@@ -1,6 +1,7 @@
 REM Si no hay dlls compilados en Build, compilar
-IF not exist Build/bin/release/OgreMain.dll (
-IF not exist Build/bin/debug/OgreMain_d.dll (
+IF not exist ../../bin/OgreMain.dll (
+IF not exist ../../bin/OgreMain_d.dll (
+IF not exist ../../bin/zlib.dll (
 rmdir /S /Q Build 
 mkdir Build
 cd Build
@@ -8,6 +9,8 @@ cmake ../Src
 cmake --build . --config release 
 cmake --build . --config debug
 cd ..
+echo CMADE
+)
 )
 )
 REM Hacer bin si no existe ya
@@ -15,10 +18,11 @@ cd ../..
 IF not exist bin (
 mkdir bin 
 )
+echo BINDONE
 REM Copiar los dlls pertinentes
 cd dependencies/Ogre/Build/bin/debug
 copy /Y OgreMain_d.dll "../../../../../bin/OgreMain_d.dll"
 cd ../release
 copy /Y OgreMain.dll "../../../../../bin/OgreMain.dll"
 copy /Y zlib.dll "../../../../../bin/zlib.dll"
-echo DONE
+echo DLLSDONE
