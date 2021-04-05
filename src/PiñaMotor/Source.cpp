ea397a2cef6785558701276e563e0a9c7924738d
Eloy Moreno <eloymore@ucm.es>
2021-04-05 17:32:16 +0200

    Arreglada compilación de Source.cpp para Release
    Modificados mensajes de debug de MotorLoop para que solamente ocurran en Debug.
    
    Co-Authored-By: georgimednikov <48915821+georgimednikov@users.noreply.github.com>

--- src/PiñaMotor/Source.cpp
+++ src/PiñaMotor/Source.cpp
@@ -1,28 +1,10 @@
-/*
-#include <OgreRoot.h>
-#include "Entity.h"
-#include "Transform.h"
-#include "ComponentFactoryRegistration.h"
+
+//#include <OgreRoot.h>
+//#include "Entity.h"
+//#include "Transform.h"
+//#include "ComponentFactoryRegistration.h"
 #include <btBulletDynamicsCommon.h>
-#include "MotorLoop.h"
-
-
-#if (defined _DEBUG) || !(defined _WIN32) //<-- Ya no lo tenemos en teor�a
-int main() {
-#else
-#include <windows.h>
-int WINAPI
-WinMain(HINSTANCE zhInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow) {
-#endif
-_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // Check Memory Leaks
-	Ogre::Root* root;
-	root = new Ogre::Root();
-	//TODO: carne de ca�on de macro
- //   ComponentFactoryRegistrations::ComponentFactoryRegistration<Transform> cpm;
- //   Entity* ent = new Entity();
-	return 0;
-}
-/*/
+//#include "MotorLoop.h"
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -44,7 +26,21 @@ void openlualibs(lua_State* l) {
 	}
 }
 
-int main(void) {
+#if (defined _DEBUG) || !(defined _WIN32) //<-- Ya no lo tenemos en teor�a
+int main() {
+#else
+#include <windows.h>
+int WINAPI
+WinMain(HINSTANCE zhInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow) {
+#endif
+_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // Check Memory Leaks
+	//Ogre::Root* root;
+	//root = new Ogre::Root();
+	////TODO: carne de ca�on de macro
+ ////   ComponentFactoryRegistrations::ComponentFactoryRegistration<Transform> cpm;
+ ////   Entity* ent = new Entity();
+	//return 0;
+
 
 	lua_State* l;
 	l = luaL_newstate();
@@ -58,4 +54,3 @@ int main(void) {
 	return 0;
 }
 
-//*/
