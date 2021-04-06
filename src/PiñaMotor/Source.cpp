dc499d675eb7ec7123e0d09f931c66ba7ead9b6c
Daniel González Cerdeiras <daniel20@ucm.es>
2021-04-06 13:58:12 +0200

    Modificada la clase Vector3 y creada la clase Vector2
    Ahora la clase Vector3 está implementada mediante templates para aceptar varios tipos, se ha limpiado un poco el código y se ha creado una conversión de nuestro tipo Vector3 al de Ogre. A partir de esta nueva versión de Vector3 se ha implementado Vector2.

--- src/PiñaMotor/Source.cpp
+++ src/PiñaMotor/Source.cpp
@@ -3,7 +3,8 @@
 #include "Transform.h"
 #include "ComponentFactoryRegistration.h"
 #include <btBulletDynamicsCommon.h>
-#include "MotorLoop.h"
+#include "Vector3.h"
+#include "Vector2.h"
 
 #if (defined _DEBUG) || !(defined _WIN32) //<-- Ya no lo tenemos en teor�a
 int main() {
@@ -18,5 +19,34 @@ _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // Check Memory Le
     //TODO: carne de ca�on de macro
  /*   ComponentFactoryRegistrations::ComponentFactoryRegistration<Transform> cpm;
     Entity* ent = new Entity();*/
+
+    Vector3<float> vec1 = Vector3<float>(1.0, 1.5, 2.0);
+    Vector3<int> vec2 = Vector3<int>(5, 10, 15);
+    vec1 *= 5; vec1.print();
+    vec2 /= 2; vec2.print();
+    cout << vec1.magnitude() << "\n";
+    cout << vec2.magnitude() << "\n";
+    vec1.normalized(); vec2.normalized();
+    cout << vec1.getX() << " " << vec1.getY() << " " << vec1.getZ() << "\n";
+    cout << vec2.getX() << " " << vec2.getY() << " " << vec2.getZ() << "\n";
+    vec1 = Vector3<float>::forward(); vec1.print();
+    vec2 = Vector3<int>::back(); vec2.print();
+
+    Vector2<float> vec3 = Vector2<float>(1.0, 1.5);
+    Vector2<int> vec4 = Vector2<int>(5, 10);
+    vec3 *= 5; vec3.print();
+    vec4 /= 2; vec4.print();
+    cout << vec3.magnitude() << "\n";
+    cout << vec4.magnitude() << "\n";
+    vec3.normalized(); vec4.normalized();
+    cout << vec3.getX() << " " << vec3.getY() << "\n";
+    cout << vec4.getX() << " " << vec4.getY() << "\n";
+    vec3 = Vector2<float>::up(); vec3.print();
+    vec4 = Vector2<int>::down(); vec4.print();
+
+    Ogre::Vector3 ogrevec = (Ogre::Vector3)Vector3<float>::forward();
+    cout << ogrevec.x << " " << ogrevec.y << " " << ogrevec.z << "\n";
+
+    delete root;
     return 0;
 }
---
+++ src/PiñaMotor/Vector2.h
@@ -0,0 +1,136 @@
+#pragma once
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;
+
+/*
+*	Se declara toda la clase en el .h para evitar problemas de link.
+*	Al utilizar templates en una clase dividida entre .h y .cpp el
+*	compilador se pierde un poco y las soluciones que he visto no parecian muy limpias.
+*/
+
+
+template <typename T = float>
+class Vector2 {
+public:
+	// Constructor vacío (inicia todas las variables a 0)
+	Vector2() {
+		_x = _y = 0;
+	}
+
+	// Constructor por parámetros
+	Vector2(T x, T y) :
+		_x(x), _y(y) {}
+
+	// Constructor por copia
+	Vector2(const Vector2<T>& other) :
+		_x(other.getX()), _y(other.getY()){}
+
+	// Destructor (vacío)
+	~Vector2() {}
+
+	// Getters
+	inline T getX() const { return _x; };
+	inline T getY() const { return _y; };
+
+	// Setters
+	inline void setX(T x) { _x = x; };
+	inline void setY(T y) { _y = y; };
+
+	// Métodos
+	double magnitudeSquared() const { return (sqrt(_x) + sqrt(_y)); }
+	double magnitude() const { return sqrt(magnitudeSquared()); }
+
+	/// <summary>
+	/// Normaliza el vector
+	/// </summary>
+	void normalized() {
+		double length = magnitude();
+		if (length > 0) *this /= (T)length;
+		else _x = _y = 0;
+	}
+
+	/// <summary>
+	/// Devuelve el producto escalar entre 2 vectores
+	/// </summary>
+	double dotProduct(const Vector2<T>& other) const { return _x * other.getX() + _y * other.getY(); }
+
+	/// <summary>
+	/// Devuelve true si todas las variables del vector están a 0
+	/// </summary>
+	bool isZero() const { return ((_x == 0) && (_y == 0)); }
+
+	/// <summary>
+	/// Setea el vector a 0
+	/// </summary>
+	void clear() { _x = _y = 0; }
+
+	/// <summary>
+	/// Invierte el vector
+	/// </summary>
+	void inverse() { _x = -_x; _y = -_y; }
+
+	/// <summary>
+	/// Calcula el ángulo con respecto a otro vector
+	/// </summary>
+	/// <returns> Ángulo en radianes </returns>
+	double angleRadians(const Vector2<T>& other) const {
+		double dot = dotProduct(other);
+		double lenSq1 = magnitudeSquared();
+		double lenSq2 = other.magnitudeSquared();
+		double angle = acos(dot / sqrt(lenSq1 * lenSq2));
+		return angle;
+	}
+
+	/// <summary>
+	/// Calcula el ángulo con respecto a otro vector
+	/// </summary>
+	/// <returns> Ángulo en grados </returns>
+	double angleDegrees(const Vector2<T>& other) const {
+		double angle = angleRadians(other);
+		angle = angle * 180 / M_PI;
+		return angle;
+	}
+
+	// Operadores
+	bool operator==(const Vector2<T>& other) const {
+		// Si comparten la misma dirección de memoria es que son el mismo vector
+		if (this == &other) return true;
+		// Si las tres componentes son iguales también son el mismo vector
+		if ((_x == other.getX()) && (_y == other.getY())) return true;
+		return false;
+	}
+	bool operator!=(const Vector2<T>& other) const { return !(*this == other); }
+
+	Vector2<T>& operator+=(const Vector2<T>& other) { _x += other.getX(); _y += other.getY(); return *this; }
+	Vector2<T> operator+(const Vector2<T>& other) const { return Vector2<T>(_x, _y) += other; }
+
+	Vector2<T>& operator-=(const Vector2<T>& other) { _x -= other.getX(); _y -= other.getY(); return *this; }
+	Vector2<T> operator-(const Vector2<T>& other) const { return Vector2<T>(_x, _y) -= other; }
+
+	Vector2& operator*=(const T val) { _x *= val; _y *= val; return *this; }
+	Vector2<T> operator*(const T val) const { return Vector2<T>(_x, _y) *= val; }
+
+	Vector2<T>& operator/=(const T val) { _x /= val; _y /= val; return *this; }
+	Vector2<T> operator/(const T val) const { return Vector2<T>(_x, _y) /= val; }
+
+	//Parsear de Vector2 a Ogre::Vector2
+	operator Ogre::Vector2() const { return Ogre::Vector2((float)_x, (float)_y); }
+
+	// DEBUG
+	void print() { cout << "(" << _x << ", " << _y << ")" << "\n"; };
+
+	// Vectores predefinidos
+	static const Vector2<T> up() { return Vector2<T>((T)0, (T)1); }
+	static const Vector2<T> down() { return Vector2<T>((T)0, (T)-1); }
+	static const Vector2<T> left() { return Vector2<T>((T)-1, (T)0); }
+	static const Vector2<T> right() { return Vector2<T>((T)1, (T)0); }
+	static const Vector2<T> one() { return Vector2<T>((T)1, (T)1); }
+	static const Vector2<T> zero() { return Vector2<T>((T)0, (T)0); }
+
+private:
+	// Variables
+	T _x;
+	T _y;
+};
