62d075fee39780c426b5218663e044816e27d2ac
Paula Martínez <pamart24@ucm.es>
2021-04-12 19:37:09 +0200

    Corregidos errores y cambiado el setParent

--- src/PiñaMotor/Transform.cpp
+++ src/PiñaMotor/Transform.cpp
@@ -23,6 +23,10 @@ void Transform::translate(float x, float y, float z) {
 	_localPosition = { _localPosition.getX() + x,
 					   _localPosition.getY() + y, 
 					   _localPosition.getZ() + z };
+
+	for (auto c : _vChild) {
+		c->translate(x, y, z);
+	}
 }
 
 void Transform::rotate(float xAngle, float yAngle, float zAngle, Space relativeTo) {
@@ -41,10 +45,10 @@ void Transform::rotate(float xAngle, float yAngle, float zAngle, Space relativeT
 	else {
 		// Transladamos el obj al centro (guardamos la direccion)
 		Vector3 dir = _position;
-		if (relativeTo == Space::World)
-			setPosition(0, 0, 0);
-		else
+		if (relativeTo == Space::Parent && parent != nullptr)
 			setPosition(_parent->position());
+		else
+			setPosition(0, 0, 0);
 
 		// Rotamos x, y, z angulo
 		rotate(xAngle, yAngle, zAngle, Space::Self);
@@ -56,6 +60,10 @@ void Transform::rotate(float xAngle, float yAngle, float zAngle, Space relativeT
 		// Volvemos a transladar el obj habiendo rotado tb la direccion
 		setPosition(newDir);
 	}
+
+	for (auto c : _vChild) {
+		c->rotate(xAngle, yAngle, zAngle, relativeTo);
+	}
 }
 
 Transform* Transform::findChild(char* name) {
@@ -66,94 +74,174 @@ Transform* Transform::findChild(char* name) {
 	return nullptr;
 }
 
+void Transform::setParent(Transform* parent)
+{
+	// Si teniamos otro padre, nos quitamos como hijos
+	if (_parent != nullptr)
+		_parent->removeChild(this);
+
+	// Nos ponemos como hijos del nuevo padre
+	if (parent != nullptr)
+		parent->setChild(this);
+
+	_parent = parent;
+	_localPosition = inverseTransformDirection(_position);
+	_localRotation = inverseTransformDirection(_rotation);
+	_localScale = inverseTransformDirection(_scale);
+}
+
 void Transform::setPosition(Vector3 v) {
 	_position = v;
 	_localPosition = inverseTransformDirection(v);
+
+	for (auto c : _vChild) {
+		c->setPosition(v + c->localPosition());
+	}
 }
 
 void Transform::setPosition(float x, float y, float z) {
 	_position = { x, y, z };
 	_localPosition = inverseTransformDirection(x, y, z);
+
+	for (auto c : _vChild) {
+		c->setPosition(Vector3(x, y, z) + c->localPosition());
+	}
 }
 
 void Transform::setRotation(Vector3 v) {
 	_rotation = v;
 	_localRotation = inverseTransformDirection(v);
+
+	for (auto c : _vChild) {
+		c->setRotation(v + c->localRotation());
+	}
 }
 
 void Transform::setRotation(float x, float y, float z) {
 	_rotation = { x, y, z };
 	_localRotation = inverseTransformDirection(x, y, z);
+
+	for (auto c : _vChild) {
+		c->setRotation(Vector3(x, y, z) + c->localRotation());
+	}
 }
 
 void Transform::setScale(Vector3 v)
 {
 	_scale = v;
 	_localScale = inverseTransformDirection(v);
+
+	for (auto c : _vChild) {
+		c->setScale(v + c->localScale());
+	}
 }
 
 void Transform::setScale(float x, float y, float z)
 {
 	_scale = { x, y, z };
 	_localScale = inverseTransformDirection(x, y, z);
+
+	for (auto c : _vChild) {
+		c->setScale(Vector3(x, y, z) + c->localScale());
+	}
 }
 
 void Transform::setLocalPosition(Vector3 v)
 {
 	_localPosition = v;
 	_position = transformDirection(v);
+
+	for (auto c : _vChild) {
+		c->setPosition(_position + c->localPosition());
+	}
 }
 
 void Transform::setLocalPosition(float x, float y, float z)
 {
 	_localPosition = { x, y, z };
 	_position = transformDirection(x, y, z);
+
+	for (auto c : _vChild) {
+		c->setPosition(_position + c->localPosition());
+	}
 }
 
 void Transform::setLocalRotation(Vector3 v)
 {
 	_localRotation = v;
 	_rotation = transformDirection(v);
+
+	for (auto c : _vChild) {
+		c->setRotation(_rotation + c->localRotation());
+	}
 }
 
 void Transform::setLocalRotation(float x, float y, float z)
 {
 	_localRotation = { x, y, z };
 	_rotation = transformDirection(x, y, z);
+
+	for (auto c : _vChild) {
+		c->setRotation(_rotation + c->localRotation());
+	}
 }
 
 void Transform::setLocalScale(Vector3 v)
 {
 	_localScale = v;
 	_scale = transformDirection(v);
+
+	for (auto c : _vChild) {
+		c->setScale(_scale + c->localScale());
+	}
 }
 
 void Transform::setLocalScale(float x, float y, float z)
 {
 	_localScale = { x, y, z };
 	_scale = transformDirection(x, y, z);
+
+	for (auto c : _vChild) {
+		c->setScale(_scale + c->localScale());
+	}
 }
 
 Vector3 Transform::transformDirection(Vector3 direction) {
-	return { abs(direction.getX() + _parent->localPosition().getX()),
-			 abs(direction.getY() + _parent->localPosition().getY()),
-			 abs(direction.getZ() + _parent->localPosition().getZ()) };
+	if (parent == nullptr)
+		return direction;
+
+	else
+		return { abs(direction.getX() + _parent->localPosition().getX()),
+				 abs(direction.getY() + _parent->localPosition().getY()),
+				 abs(direction.getZ() + _parent->localPosition().getZ()) };
 }
 
 Vector3 Transform::transformDirection(float x, float y, float z) {
-	return { abs(x + _parent->localPosition().getX()),
-			 abs(y + _parent->localPosition().getY()),
-			 abs(z + _parent->localPosition().getZ()) };
+	if (parent == nullptr)
+		return {x, y, z};
+
+	else
+		return { abs(x + _parent->localPosition().getX()),
+				 abs(y + _parent->localPosition().getY()),
+				 abs(z + _parent->localPosition().getZ()) };
 }
 
 Vector3 Transform::inverseTransformDirection(Vector3 direction) {
-	return { abs(direction.getX() - _parent->localPosition().getX()),
-			 abs(direction.getY() - _parent->localPosition().getY()),
-			 abs(direction.getZ() - _parent->localPosition().getZ()) };
+	if (parent == nullptr)
+		return direction;
+
+	else
+		return { abs(direction.getX() - _parent->localPosition().getX()),
+				 abs(direction.getY() - _parent->localPosition().getY()),
+				 abs(direction.getZ() - _parent->localPosition().getZ()) };
 }
 
 Vector3 Transform::inverseTransformDirection(float x, float y, float z) {
-	return { abs(x - _parent->localPosition().getX()),
-			 abs(y - _parent->localPosition().getY()),
-			 abs(z - _parent->localPosition().getZ()) };
+	if (parent == nullptr)
+		return { x, y, z };
+
+	else
+		return { abs(x - _parent->localPosition().getX()),
+				 abs(y - _parent->localPosition().getY()),
+				 abs(z - _parent->localPosition().getZ()) };
 }
--- src/PiñaMotor/Transform.h
+++ src/PiñaMotor/Transform.h
@@ -12,36 +12,35 @@ public:
 	};
 
 	Transform(Vector3 position, Vector3 rotation, Vector3 scale, Transform* parent = nullptr);
-	
+
 	bool init(const std::map<std::string, std::string>& mapa) override {
 		return true;
 	}
 
 	void translate(float x, float y, float z);
 	void rotate(float xAngle, float yAngle, float zAngle, Space relativeTo = Space::Self);
-	
+
 	// Getter
 	Transform* parent() { return _parent; }
 	Transform* findChild(char* name);
-	
+
 	Vector3 position() { return _position; }
 	Vector3 rotation() { return _rotation; }
 	Vector3 scale() { return _scale; }
-	
+
 	Vector3 localPosition() { return _localPosition; }
 	Vector3 localRotation() { return _localRotation; }
 	Vector3 localScale() { return _localScale; }
-	
+
 	// Setter
-	void setParent(Transform* parent) { _parent = parent; }
-	void setChild(Transform* child) { _vChild.push_back(child); }
-	
+	void setParent(Transform* parent);
+
 	void setPosition(Vector3 v);
 	void setPosition(float x, float y, float z);
-	
+
 	void setRotation(Vector3 v);
 	void setRotation(float x, float y, float z);
-	
+
 	void setScale(Vector3 v);
 	void setScale(float x, float y, float z);
 
@@ -53,21 +52,27 @@ public:
 
 	void setLocalScale(Vector3 v);
 	void setLocalScale(float x, float y, float z);
-	
+
+
 	// Transforma el vector direction del espacio local al espacio global
 	Vector3 transformDirection(Vector3 direction);
 	// Transforma la posicion x, y, z del espacio local al espacio global
 	Vector3 transformDirection(float x, float y, float z);
-	
+
 	// Transforma el vector direction del espacio global al espacio local
 	Vector3 inverseTransformDirection(Vector3 direction);
 	// Transforma la posicion x, y, z del espacio global al espacio local
 	Vector3 inverseTransformDirection(float x, float y, float z);
 
 private:
+	// Añade a un hijo a la lista
+	void setChild(Transform* child) { _vChild.push_back(child); }
+	// Elimina a un hijo de la lista
+	void removeChild(Transform* child) { if (child->parent() == this) _vChild.remove(child); };
+
 	Transform* _parent = nullptr;
 	list<Transform*> _vChild;
-	
+
 	// En funcion del mundo (global)
 	Vector3 _position;
 	Vector3 _rotation;
