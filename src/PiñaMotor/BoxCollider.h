6ead03138828405e744475ca2e17909e1e628990
Daniel Cortijo <dcortijo@ucm.es>
2021-04-13 19:16:49 +0200

    Arreglos templates de BoxCollider

--- src/PiñaMotor/BoxCollider.h
+++ src/PiñaMotor/BoxCollider.h
@@ -7,7 +7,7 @@
 class BoxCollider : public Component {
 public:
 	// Constructora
-	BoxCollider(Vector3 position, Vector3 size, bool isTrigger = false);
+	BoxCollider(Vector3<float> position, Vector3<float> size, bool isTrigger = false);
 
 	// TODO
 	bool init(const std::map<std::string, std::string>& mapa) override {
@@ -16,16 +16,16 @@ public:
 
 	// Getters
 	inline bool getIsTrigger() const { return _isTrigger; }
-	inline Vector3 getCenter() const { return _center; }
-	inline Vector3 getSize() const { return _size; }
+	inline Vector3<float> getCenter() const { return _center; }
+	inline Vector3<float> getSize() const { return _size; }
 
 	// Setters
 	inline void setIsTrigger(bool b) { _isTrigger = b; }
 
-	inline void setCenter(Vector3 v) { _center = v; };
+	inline void setCenter(Vector3<float> v) { _center = v; };
 	inline void setCenter(float x, float y, float z) { _center = { x, y, z }; };
 
-	inline void setSize(Vector3 v) { _size = v; };
+	inline void setSize(Vector3<float> v) { _size = v; };
 	inline void setSize(float x, float y, float z) { _size = { x, y, z }; };
 	
 private:
@@ -33,10 +33,10 @@ private:
 	bool _isTrigger;
 
 	// El centro del BoxCollider
-	Vector3 _center;
+	Vector3<float> _center;
 
 	// El tamaño de cada eje (la mitad para cada lado del centro)
-	Vector3 _size;
+	Vector3<float> _size;
 
 	btCollisionObject* _btCollisionObject = nullptr;
 	btCollisionShape* _btCollisionShape = nullptr;
