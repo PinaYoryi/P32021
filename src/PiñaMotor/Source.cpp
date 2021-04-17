3854ca5704793058a1576ec43ab54c86609625ba
Eloy Moreno <eloymore@ucm.es>
2021-04-17 13:08:31 +0200

    Creada carga de archivos con LUA
    Declarado el formato a usar y creada pequeña prueba de concepto en Source.cpp. 
    Para probar: copiar el archivo dependencies/Lua/lib/myscript.lua a la carpeta bin y ejecutar.
    Si hay alguna duda, mandar un mensaje

--- src/PiñaMotor/Source.cpp
+++ src/PiñaMotor/Source.cpp
@@ -11,6 +11,9 @@
 #include <lua.hpp>
 #include <lauxlib.h>
 #include <lualib.h>
+#include <string>
+#include <iostream>
+using namespace std;
 
 static const luaL_Reg lualibs[] = {
 		{ "base",       luaopen_base },
@@ -33,13 +36,13 @@ int main() {
 int WINAPI
 WinMain(HINSTANCE zhInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow) {
 #endif
-_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // Check Memory Leaks
-	//Ogre::Root* root;
-	//root = new Ogre::Root();
-	////TODO: carne de ca�on de macro
- ////   ComponentFactoryRegistrations::ComponentFactoryRegistration<Transform> cpm;
- ////   Entity* ent = new Entity();
-	//return 0;
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // Check Memory Leaks
+		//Ogre::Root* root;
+		//root = new Ogre::Root();
+		////TODO: carne de ca�on de macro
+	 ////   ComponentFactoryRegistrations::ComponentFactoryRegistration<Transform> cpm;
+	 ////   Entity* ent = new Entity();
+		//return 0;
 
 
 	lua_State* l;
@@ -47,7 +50,33 @@ _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // Check Memory Le
 	openlualibs(l);
 
 	printf("now calling lua\n\n");
-	luaL_dofile(l, "myscript.lua");
+	luaL_loadfile(l, "myscript.lua");
+	if (lua_pcall(l, 0, 0, 0)) cout << "No va\n";
+	lua_getglobal(l, "GetMapa");
+	int err = lua_pcall(l, 0, 1, 0); // GetMapa()
+	bool b = lua_istable(l, 1); // La tabla
+
+	lua_pushnil(l);
+	while (lua_next(l, 1) != 0) {
+		// Entity is here
+		// Name
+		lua_getfield(l, -1, "name");
+		cout << lua_tostring(l, -1) << " ";
+		lua_pop(l, 1);
+
+		// ID
+		lua_getfield(l, -1, "id");
+		cout << lua_tonumber(l, -1) << "\n";
+		lua_pop(l, 1);
+
+		// Components
+		// Calls a similar while loop, creating a map<string, string> with each pair
+		// Knows which component is by key name and a traductor function
+
+		// Entity is no longer here, only key to be removed by lua_next
+		lua_pop(l, 1);
+	}
+
 	printf("\ndo something else\n\n");
 
 	lua_close(l);
