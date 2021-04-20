5502ec40e78bcb2656eb9f46826312c36d389b10
Daniel Cortijo <dcortijo@ucm.es>
2021-04-20 13:16:39 +0200

    Hecho que Ogre compila RTShaders

--- dependencies/compilar.bat
+++ dependencies/compilar.bat
@@ -13,6 +13,8 @@ if not exist ../../bin/OgrePaging.dll goto foo
 if not exist ../../bin/OgrePaging_d.dll goto foo
 if not exist ../../bin/OgreProperty.dll goto foo
 if not exist ../../bin/OgreProperty_d.dll goto foo
+if not exist ../../bin/OgreRTShaderSystem.dll goto foo
+if not exist ../../bin/OgreRTShaderSystem_d.dll goto foo
 if not exist ../../bin/Plugin_ParticleFX.dll goto
 if not exist ../../bin/Plugin_ParticleFX_d.dll goto foo
 if not exist ../../bin/plugins.cfg goto foo
@@ -27,7 +29,7 @@ echo STARTMAKE
 rmdir /S /Q Build 
 mkdir Build
 cd Build
-..\..\Cmake\cmake-3.20.0-rc2-windows-x86_64\bin\cmake.exe -DOGRE_BUILD_RENDERSYSTEM_D3D9=FALSE -DOGRE_BUILD_RENDERSYSTEM_GL3PLUS=FALSE -DOGRE_BUILD_RENDERSYSTEM_GL=FALSE -DOGRE_BUILD_RENDERSYSTEM_GLES2=FALSE -DOGRE_BUILD_PLUGIN_ASSIMP=FALSE -DOGRE_BUILD_PLUGIN_BSP=FALSE -DOGRE_BUILD_PLUGIN_OCTREE=FALSE -DOGRE_BUILD_PLUGIN_DOT_SCENE=FALSE -DOGRE_BUILD_PLUGIN_PCZ=FALSE -DOGRE_BUILD_COMPONENT_TERRAIN=FALSE -DOGRE_BUILD_COMPONENT_VOLUME=FALSE -DOGRE_BUILD_COMPONENT_BITES=FALSE -DOGRE_BUILD_COMPONENT_PYTHON=FALSE -DOGRE_BUILD_COMPONENT_JAVA=FALSE -DOGRE_BUILD_COMPONENT_CSHARP=FALSE -DOGRE_BUILD_COMPONENT_RTSHADERSYSTEM=FALSE -DOGRE_BUILD_RTSHADERSYSTEM_SHADERS=FALSE -DOGRE_INSTALL_CMAKE=FALSE -DOGRE_INSTALL_SAMPLES=FALSE -DOGRE_INSTALL_DOCS=FALSE -DOGRE_INSTALL_PDB=FALSE -DOGRE_INSTALL_VSPROPS=TRUE -DOGRE_BUILD_TOOLS=FALSE ../Src
+..\..\Cmake\cmake-3.20.0-rc2-windows-x86_64\bin\cmake.exe -DOGRE_BUILD_RENDERSYSTEM_D3D9=FALSE -DOGRE_BUILD_RENDERSYSTEM_GL3PLUS=FALSE -DOGRE_BUILD_RENDERSYSTEM_GL=FALSE -DOGRE_BUILD_RENDERSYSTEM_GLES2=FALSE -DOGRE_BUILD_PLUGIN_ASSIMP=FALSE -DOGRE_BUILD_PLUGIN_BSP=FALSE -DOGRE_BUILD_PLUGIN_OCTREE=FALSE -DOGRE_BUILD_PLUGIN_DOT_SCENE=FALSE -DOGRE_BUILD_PLUGIN_PCZ=FALSE -DOGRE_BUILD_COMPONENT_TERRAIN=FALSE -DOGRE_BUILD_COMPONENT_VOLUME=FALSE -DOGRE_BUILD_COMPONENT_BITES=FALSE -DOGRE_BUILD_COMPONENT_PYTHON=FALSE -DOGRE_BUILD_COMPONENT_JAVA=FALSE -DOGRE_BUILD_COMPONENT_CSHARP=FALSE -DOGRE_INSTALL_CMAKE=FALSE -DOGRE_INSTALL_SAMPLES=FALSE -DOGRE_INSTALL_DOCS=FALSE -DOGRE_INSTALL_PDB=FALSE -DOGRE_INSTALL_VSPROPS=TRUE -DOGRE_BUILD_TOOLS=FALSE ../Src
 ..\..\Cmake\cmake-3.20.0-rc2-windows-x86_64\bin\cmake.exe --build . --config release 
 ..\..\Cmake\cmake-3.20.0-rc2-windows-x86_64\bin\cmake.exe --build . --config debug
 cd ..
@@ -47,6 +49,7 @@ copy /Y OgreMeshLodGenerator_d.dll "../../../../../bin/OgreMeshLodGenerator_d.dl
 copy /Y OgreOverlay_d.dll "../../../../../bin/OgreOverlay_d.dll"
 copy /Y OgrePaging_d.dll "../../../../../bin/OgrePaging_d.dll"
 copy /Y OgreProperty_d.dll "../../../../../bin/OgreProperty_d.dll"
+copy /Y OgreRTShaderSystem_d.dll "../../../../../bin/OgreRTShaderSystem_d.dll"
 copy /Y Plugin_ParticleFX_d.dll "../../../../../bin/Plugin_ParticleFX_d.dll"
 copy /Y RenderSystem_Direct3D11_d.dll "../../../../../bin/RenderSystem_Direct3D11_d.dll"
 copy /Y plugins.cfg "../../../../../bin/plugins.cfg"
@@ -58,6 +61,7 @@ copy /Y OgreMeshLodGenerator.dll "../../../../../bin/OgreMeshLodGenerator.dll"
 copy /Y OgreOverlay.dll "../../../../../bin/OgreOverlay.dll"
 copy /Y OgrePaging.dll "../../../../../bin/OgrePaging.dll"
 copy /Y OgreProperty.dll "../../../../../bin/OgreProperty.dll"
+copy /Y OgreRTShaderSystem.dll "../../../../../bin/OgreRTShaderSystem.dll"
 copy /Y Plugin_ParticleFX.dll "../../../../../bin/Plugin_ParticleFX.dll"
 copy /Y RenderSystem_Direct3D11.dll "../../../../../bin/RenderSystem_Direct3D11.dll"
 copy /Y SDL2.dll "../../../../../bin/SDL2.dll"
