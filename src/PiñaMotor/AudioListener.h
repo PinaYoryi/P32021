1ddde15e42a8ddb0f90dd3dc6221392441fe3f23
Xexu98 <jesusf05@ucm.es>
2021-04-20 10:33:38 +0200

    Añdidos metodos get y set a AudioListener y Source y cambiado los ficheros a src

--- src/PiñaMotor/AudioListener.h
+++ src/PiñaMotor/AudioListener.h
@@ -2,22 +2,34 @@
 #include "Component.h"
 #include <fmod.hpp>
 #include "Vector3.h"
+#include "Audio.h"
 
 class AudioListener : public Component {
 
 
 public:
 	AudioListener() {};
+	AudioListener(Vector3<float> position, Vector3<float> velocity);
 	~AudioListener() {};
 
 	bool init(const std::map<std::string, std::string>& mapa) override {
 		return true;
 	}
 
-	void setPosition() {};
-	Vector3<float> getPosition() { return _position; };
+	void setPosition(Vector3<float> position) { _position.x = position.getX(); _position.y = position.getY(); _position.z = position.getZ();};
+	Vector3<float> getPosition() { return Vector3<float>(_position.x,_position.y,_position.z); };
+
+	void setVelocity(Vector3<float> velocity) { _velocity.x = velocity.getX(); _velocity.y = velocity.getY(); _velocity.z = velocity.getZ(); };
+	Vector3<float> getPosition() { return Vector3<float>(_velocity.x, _velocity.y, _velocity.z); };
+
+	void update();
+
 
 private:
 
-	Vector3<float> _position;
+	FMOD::System* _system;
+	FMOD_VECTOR _position;
+	FMOD_VECTOR _velocity;
+	FMOD_VECTOR _forward;
+	FMOD_VECTOR _up;
 };
--- src/PiñaMotor/AudioSource.h
+++ src/PiñaMotor/AudioSource.h
@@ -3,6 +3,7 @@
 #include <fmod.hpp>
 #include <map>
 #include "Vector3.h"
+#include "Audio.h"
 
 class AudioSource : public Component {
 public:
@@ -20,12 +21,13 @@ public:
 	// Añade un nuevo sonido a la lista
 	void addNewSound(const std::string name);
 
-	void setPosition();
-	Vector3<float> getPosition() { return _position; };
+	void setPosition(Vector3<float> position) { _position.x = position.getX(); _position.x = position.getX(); _position.x = position.getX(); };
+	Vector3<float> getPosition() { return Vector3<float>(_position.x, _position.y, _position.z); };
 
 private:
 	
-	Vector3<float> _position;
+	FMOD_VECTOR _position;
+	FMOD_VECTOR _velocity;
 	// Lista con los canales de sonido que se estan ejecutando
 	std::map<std::string, FMOD::Channel*> _sounds;
 };
