66bd0c90493929d0b3a8218c1df2ac28e11deb39
Daniel Cortijo <dcortijo@ucm.es>
2021-04-23 18:33:24 +0200

    .bats de LUA y FMOD

--- dependencies/compilar.bat
+++ dependencies/compilar.bat
@@ -1,31 +1,31 @@
 REM OGRE
-REM Si no hay dlls compilados en Build, compilar
+echo CHECKOGRE
 cd Ogre
-if not exist ../../bin/Codec_STBI.dll goto foo
-if not exist ../../bin/Codec_STBI_d.dll goto foo
-if not exist ../../bin/OgreMain.dll goto foo
-if not exist ../../bin/OgreMain_d.dll goto foo
-if not exist ../../bin/OgreMeshLodGenerator.dll goto foo
-if not exist ../../bin/OgreMeshLodGenerator_d.dll goto foo
-if not exist ../../bin/OgreOverlay.dll goto foo
-if not exist ../../bin/OgreOverlay_d.dll goto foo
-if not exist ../../bin/OgrePaging.dll goto foo
-if not exist ../../bin/OgrePaging_d.dll goto foo
-if not exist ../../bin/OgreProperty.dll goto foo
-if not exist ../../bin/OgreProperty_d.dll goto foo
-if not exist ../../bin/OgreRTShaderSystem.dll goto foo
-if not exist ../../bin/OgreRTShaderSystem_d.dll goto foo
+if not exist ../../bin/Codec_STBI.dll goto ogro
+if not exist ../../bin/Codec_STBI_d.dll goto ogro
+if not exist ../../bin/OgreMain.dll goto ogro
+if not exist ../../bin/OgreMain_d.dll goto ogro
+if not exist ../../bin/OgreMeshLodGenerator.dll goto ogro
+if not exist ../../bin/OgreMeshLodGenerator_d.dll goto ogro
+if not exist ../../bin/OgreOverlay.dll goto ogro
+if not exist ../../bin/OgreOverlay_d.dll goto ogro
+if not exist ../../bin/OgrePaging.dll goto ogro
+if not exist ../../bin/OgrePaging_d.dll goto ogro
+if not exist ../../bin/OgreProperty.dll goto ogro
+if not exist ../../bin/OgreProperty_d.dll goto ogro
+if not exist ../../bin/OgreRTShaderSystem.dll goto ogro
+if not exist ../../bin/OgreRTShaderSystem_d.dll goto ogro
 if not exist ../../bin/Plugin_ParticleFX.dll goto
-if not exist ../../bin/Plugin_ParticleFX_d.dll goto foo
-if not exist ../../bin/plugins.cfg goto foo
-if not exist ../../bin/RenderSystem_Direct3D11.dll goto foo
-if not exist ../../bin/RenderSystem_Direct3D11_d.dll goto foo
-if not exist ../../bin/resources.cfg goto foo
-if not exist ../../bin/SDL2.dll goto foo
-if not exist ../../bin/zlib.dll goto foo
-goto afterfoo
-:foo
-echo STARTMAKE
+if not exist ../../bin/Plugin_ParticleFX_d.dll goto ogro
+if not exist ../../bin/plugins.cfg goto ogro
+if not exist ../../bin/RenderSystem_Direct3D11.dll goto ogro
+if not exist ../../bin/RenderSystem_Direct3D11_d.dll goto ogro
+if not exist ../../bin/resources.cfg goto ogro
+if not exist ../../bin/SDL2.dll goto ogro
+if not exist ../../bin/zlib.dll goto ogro
+goto afterogro
+:ogro
+echo MAKEOGRE
 rmdir /S /Q Build 
 mkdir Build
 cd Build
@@ -33,15 +33,12 @@ cd Build
 ..\..\Cmake\cmake-3.20.0-rc2-windows-x86_64\bin\cmake.exe --build . --config release 
 ..\..\Cmake\cmake-3.20.0-rc2-windows-x86_64\bin\cmake.exe --build . --config debug
 cd ..
-echo CMADE
-:afterfoo
-REM Hacer bin si no existe ya
+echo CMADEOGRE
 cd ../..
 IF not exist bin (
 mkdir bin 
 echo BINDONE
 )
-REM Copiar los dlls pertinentes
 cd dependencies/Ogre/Build/bin/debug
 copy /Y Codec_STBI_d.dll "../../../../../bin/Codec_STBI_d.dll"
 copy /Y OgreMain_d.dll "../../../../../bin/OgreMain_d.dll"
@@ -66,22 +63,48 @@ copy /Y Plugin_ParticleFX.dll "../../../../../bin/Plugin_ParticleFX.dll"
 copy /Y RenderSystem_Direct3D11.dll "../../../../../bin/RenderSystem_Direct3D11.dll"
 copy /Y SDL2.dll "../../../../../bin/SDL2.dll"
 copy /Y zlib.dll "../../../../../bin/zlib.dll"
-echo COPYDONE
+cd ../../../
+:afterogro
+echo DOGRE
+
 REM BULLET
-REM Si no hay libs compilados en build, compilar
-echo COMPROBATION
-cd ../../../../Bullet
-if not exist build/lib goto foo
-goto afterfoo
-:foo
-echo STARTMAKE
+echo CHECKBULLET
+cd ../Bullet
+if not exist build/lib goto bala
+goto afterbala
+:bala
+echo MAKEBULLET
 rmdir /S /Q build 
 mkdir build
 cd build
 ..\..\Cmake\cmake-3.20.0-rc2-windows-x86_64\bin\cmake.exe -DUSE_MSVC_RUNTIME_LIBRARY_DLL=ON -DBUILD_BULLET2_DEMOS=OFF -DBUILD_CLSOCKET=OFF -DBUILD_CPU_DEMOS=OFF -DBUILD_ENET=OFF -DBUILD_UNIT_TESTS=OFF -DENABLE_VHACD=OFF -DINSTALL_LIBS=OFF -DUSE_GLUT=OFF -DUSE_GRAPHICAL_BENCHMARK=OFF -DBUILD_OPENGL3_DEMOS=OFF ../src
 ..\..\Cmake\cmake-3.20.0-rc2-windows-x86_64\bin\cmake.exe --build . --config release 
 ..\..\Cmake\cmake-3.20.0-rc2-windows-x86_64\bin\cmake.exe --build . --config debug
-cd ..
-echo CMADE
-:afterfoo
-REM Aqui no hay que copiar nada
+cd ../
+echo CMADEBULLET
+:afterbala
+echo DONEBULLET
+
+REM LUA
+echo CHECKLUA
+cd ../Lua
+if not exist ../../bin/lua54.dll goto lula
+goto afterlula
+:lula
+cd lib
+copy /Y lua54.dll "../../../bin/lua54.dll"
+cd ../
+:afterlula
+echo DONELUA
+
+REM FMOD
+echo CHECKFMOD
+cd ../FMOD
+if not exist ../../bin/fmodL64.dll goto sound
+goto aftersound
+:sound
+cd lib
+copy /Y fmodL64.dll "../../../bin/fmodL64.dll"
+cd ../
+:aftersound
+echo FMODONE
