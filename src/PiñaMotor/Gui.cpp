09c5debb0c2508364bf4cea85afff58af6abba26
jossan10 <jossan10@ucm.es>
2021-05-13 21:03:40 +0200

    Cegui inicializacion
    Hemos incluido las librerias en propiedades del motor
    Hemos tenido que establecer dos rutas para acceder al include de Cegui para tener accesible Config.h
    Y en dependencias adicionales de Library añadir las librerias alojadas en el directorio
    
    Falta testear la clase GUI para asegurar  su funcionamiento.
    
    Co-Authored-By: pamart24 <48999523+pamart24@users.noreply.github.com>
    Co-Authored-By: Xexu98 <26869052+Xexu98@users.noreply.github.com>

--- src/PiñaMotor/Gui.cpp
+++ src/PiñaMotor/Gui.cpp
@@ -2,25 +2,48 @@
 #include "OgreMotor.h"
 
 Gui::Gui() {
-	//_mRenderer = &CEGUI::OgreRenderer::bootstrapSystem(*OgreSDLContext::getInstance()->getRenderTarget());
+	_mRenderer = &CEGUI::OgreRenderer::bootstrapSystem(*OgreMotor::GetInstance()->getRoot()->getRenderTarget(""));
 	_mRoot = OgreMotor::GetInstance()->getRoot();
 	_mWindow = OgreMotor::GetInstance()->getRenderWindow();
-	//mContext_ = &CEGUI::System::getSingleton().getDefaultGUIContext();
+	_mContext = &CEGUI::System::getSingleton().getDefaultGUIContext();
 
-	//CEGUI::ImageManager::setImagesetDefaultResourceGroup("NombreCarpetaImagenes");
-	//CEGUI::Font::setDefaultResourceGroup("NombreCarpetaFuentes");
-	//CEGUI::Scheme::setDefaultResourceGroup("NombreCarpetaEsquemas");
-	//CEGUI::WidgetLookManager::setDefaultResourceGroup("NombreCarpetaWidget");
-	//CEGUI::WindowManager::setDefaultResourceGroup("NombreCarpetaLayouts");
+	CEGUI::ImageManager::setImagesetDefaultResourceGroup("NombreCarpetaImagenes");
+	CEGUI::Font::setDefaultResourceGroup("NombreCarpetaFuentes");
+	CEGUI::Scheme::setDefaultResourceGroup("NombreCarpetaEsquemas");
+	CEGUI::WidgetLookManager::setDefaultResourceGroup("NombreCarpetaWidget");
+	CEGUI::WindowManager::setDefaultResourceGroup("NombreCarpetaLayouts");
 
-	//_mWindowManager = &CEGUI::WindowManager::getSingleton();
-	//_sheet = _mWindowManager->createWindow("DefaultWindow", "CEGUIDemo/Sheet");
-	//CEGUI::System::getSingleton().getDefaultGUIContext().setRootWindow(sheet_);
+	_mWindowManager = &CEGUI::WindowManager::getSingleton();
+	_sheet = _mWindowManager->createWindow("DefaultWindow", "CEGUIDemo/Sheet");
+	CEGUI::System::getSingleton().getDefaultGUIContext().setRootWindow(_sheet);
 
-	//createFrameListener();
+	createFrameListener();
 }
 
 bool Gui::Init() {
 	if (_guiInstance != nullptr) return false;
 	_guiInstance = new Gui();
+}
+
+void Gui::createFrameListener(){
+	auto windowHnd = 0;
+	std::ostringstream windowHndStr;
+
+	_mWindow->getCustomAttribute("WINDOW", &windowHnd);
+	windowHndStr << windowHnd;
+
+	// Set initial mouse clipping size
+	windowResized(_mWindow);
+
+	_mRoot->addFrameListener(this);
+}
+
+void Gui::windowResized(Ogre::RenderWindow* w){
+
+	unsigned int width = 0, height = 0, depth = 0;
+	int left = 0, top = 0;
+	w->getMetrics(width, height, depth, left, top);
+	CEGUI::Sizef newSize(width, height);
+	// mRenderer_->setDisplaySize(newSize);
+	CEGUI::System::getSingleton().notifyDisplaySizeChanged(newSize);
 }
--- src/PiñaMotor/Gui.h
+++ src/PiñaMotor/Gui.h
@@ -1,27 +1,38 @@
 #pragma once
-//#include <CEGUI/CEGUI.h>
-//#include <CEGUI/MouseCursor.h>
-//#include <CEGUI/RendererModules/Ogre/Renderer.h>
+#include <CEGUI/CEGUI.h>
+#include <CEGUI/MouseCursor.h>
+#include <CEGUI/RendererModules/Ogre/Renderer.h>
 #include <Ogre.h>
 #include <iostream>
 
 class Gui : public Ogre::FrameListener {
+
 public:
+
 	Gui();
+
 	~Gui();
 
-	static Gui* GetInstance();
+	static Gui* GetInstance() { return _guiInstance; };
 
 	static bool Init();
 
 private:
+	
+	void createFrameListener();
+	void windowResized(Ogre::RenderWindow* w);
+
 	static Gui* _guiInstance;
 
 	Ogre::RenderWindow* _mWindow = nullptr;
-	//CEGUI::OgreRenderer* _mRenderer = nullptr;
-	Ogre::Root* _mRoot = nullptr;
-	//CEGUI::GUIContext* _mContext = nullptr;
 
-	//CEGUI::Window* _sheet = nullptr;
-	//CEGUI::WindowManager* _mWindowManager = nullptr;
+	Ogre::Root* _mRoot = nullptr;
+
+	CEGUI::OgreRenderer* _mRenderer = nullptr;
+
+	CEGUI::GUIContext* _mContext = nullptr;
+
+	CEGUI::Window* _sheet = nullptr;
+
+	CEGUI::WindowManager* _mWindowManager = nullptr;
 };
