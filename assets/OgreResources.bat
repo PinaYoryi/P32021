58bbbe08252df256abbed08a1c1c203931924fb4
georgimednikov <gmedniko@ucm.es>
2021-05-28 00:02:36 +0200

    Arreglado bat de resources

--- assets/OgreResources.bat
+++ assets/OgreResources.bat
@@ -2,5 +2,5 @@ cd ../dependencies/Ogre/Build/bin/debug
 copy /Y resources.cfg "../../../../../bin/resources.cfg"
 cd ../../../../../bin/resources
 echo [General] >> ../resources.cfg
-echo FileSystem=%CD%/resources >> ../resources.cfg
+echo FileSystem=%CD% >> ../resources.cfg
 for /r %CD% %%f in (*.zip) do echo Zip=%%~pnxf >> ../resources.cfg
