a163686770029a067d0d77027a1283ea5b9d031a
jossan10 <jossan10@ucm.es>
2021-07-11 13:36:10 +0200

    cambios bat compilacion
    Cambiar rutas absolutas por rutas relativas al generar bin

--- assets/OgreResources.bat
+++
@@ -1,6 +0,0 @@
-cd ../dependencies/Ogre/Build/bin/debug
-copy /Y resources.cfg "../../../../../bin/resources.cfg"
-cd ../../../../../bin/resources
-echo [General] >> ../resources.cfg
-echo FileSystem=%CD% >> ../resources.cfg
-for /r %CD% %%f in (*.zip) do echo Zip=%%~pnxf >> ../resources.cfg
